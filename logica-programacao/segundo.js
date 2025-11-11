95f9116487e790db163d29559546e52e7fbe9df6
odudu07 <eduardo.arocha2024@gmail.com>
2025-11-11 14:09:11 -0300

    faltei

---
+++ logica-programacao/segundo.js
@@ -0,0 +1,11 @@
+let numero1 = Number(prompt("Digite um número"));
+let numero2 = Number(prompt("Digite outro número"));
+
+// soma
+console.log("Soma: " + numero1 + numero2);
+// Sub
+console.log("Sub: ", numero1 - numero2);
+// Mult
+console.log("Mult: " + numero1 * numero2);
+// Div
+console.log("Div: " + numero1 / numero2);
