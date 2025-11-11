2f22c0c4f76141c78f1911ce96101d77461191ea
odudu07 <eduardo.arocha2024@gmail.com>
2025-11-11 15:00:44 -0300

    idade

---
+++ logica-programacao/meses.js
@@ -0,0 +1,4 @@
+let  anos = prompt ('Digite sua idade em anos');
+let meses =  anos * 12;
+
+console.log("Voê tem " + meses + "meses de vida");
---
+++ logica-programacao/terceiro.js
@@ -0,0 +1,7 @@
+// Programa que calcula o dobro de um número
+
+let numero = Number(prompt("Digite um número:"));
+let dobro = numero * 2;
+
+console.log("O dobro do número é: " + dobro);
+
