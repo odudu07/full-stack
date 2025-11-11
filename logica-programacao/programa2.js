d6c60741bd8ce50d3d602ad687122fc56d5bc65a
odudu07 <eduardo.arocha2024@gmail.com>
2025-11-11 16:22:15 -0300

    programação

--- logica-programacao/programa2.js
+++ logica-programacao/programa2.js
@@ -1,8 +1,9 @@
 function mostraDobro(valor) {
-  console.log("O dobro de " + valor + " é " + valor * 2);
+  document.write("O dobro de " + valor + " é " + valor * 2);
 }
 
 let numero = Number(prompt("Digite um número: "));
 
 mostraDobro(numero);
+
 
