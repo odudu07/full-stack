1294ec50ad868b747d0d5b29293d29b56c8d3ea2
odudu07 <eduardo.arocha2024@gmail.com>
2025-11-19 13:59:31 -0300

    Edu

--- SMART-CONTATOS/js/script.js
+++ SMART-CONTATOS/js/script.js
@@ -1,4 +1,32 @@
-function btmCadsatrar() {
-  alert("Olá Turma!");
-  console.log("Olá Turma")
-}
+const form = document.querySelector("form");
+const lista = document.querySelector(".lista");
+const inputNome = document.getElementById("nome");
+const inputEmail = document.getElementById("email");
+const inputTel = document.getElementById("telefone");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Validação do formulário
+  if (inputNome.value == "" || inputEmail.value == "" || inputTel.value == "") {
+    alert("Digite seus dados");
+    return false;
+  }
+
+  // Criar LI
+  const li = document.createElement("li");
+
+  li.innerHTML = `
+        <span class="contato-nome">${inputNome.value}</span>
+        <span class="contato-email">${inputEmail.value}</span>
+        <span class="contato-telefone">${inputTel.value}</span>
+    `;
+
+  console.log(li);
+
+  // appendChild()
+  lista.appendChild(li);
+
+  // Limpar inputs
+  form.reset();
+});
